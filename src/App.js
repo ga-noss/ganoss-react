8836a9828772d4e0722bfa7ce7b90e1463ca0194
Ganoss <lgr2612@gmail.com>
2021-10-14 18:08:18 +0400

    qwerty

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+// function App() {
+  // return (
+  //   <div className="App">
+  //     <h1>Bienvenue sur mon premier projet React</h1>
+  //   </div>
+  // );
+// }
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur mon premier projet React</h1>
+    </div>
+  );
+}
+
+export default App; 
