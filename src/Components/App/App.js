8503f60b3d8b23aacfe8413f4b54ba33132c1026
Ganoss <lgr2612@gmail.com>
2021-10-15 14:20:59 +0400

    21

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,15 +4,38 @@ import './App.css';
 import Eleve from '../Eleve/Eleve';
 
 class App extends Component {
+  state = {
+    eleves: [
+      { 
+        name: 'Eva Dupont', 
+        average: 15, 
+        citation: '"Allez toujours plus loin !"' 
+      },
+      { 
+        name: 'Timothée Galo', 
+        average: 5, 
+        citation: null
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Bienvenue dans la classe Terre</h1>
-        <Eleve name="Eva Dupont" average="15">
-          "Allez toujours plus loin"
+
+        <Eleve 
+          name={this.state.eleves[0].name}  
+          average={this.state.eleves[0].average}
+        >
+          {this.state.eleves[0].citation}
         </Eleve>
-        <Eleve name="Timothée Galo" average="5">
-          "Citation"
+
+        <Eleve 
+          name={this.state.eleves[1].name} 
+          average={this.state.eleves[1].average}
+        >
+          {this.state.eleves[1].citation}
         </Eleve>
       </div>
     );
